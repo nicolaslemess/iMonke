1226204fbc9ce46021a57a9901feb8cb3d7e6337
nicolas <nicolasrussel123@gmail.com>
2021-02-10 19:45:51 -0300

    frst

--- commands/monkepedia.js
+++ commands/monkepedia.js
@@ -39,7 +39,7 @@ module.exports = {
 
         let embed6 = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('Macaco Noite')
+        .setTitle('Macaco da Noite')
         .setDescription('Macacos da noite, assim como as demais espécies de seu gênero. Essa espécie é constituída de animais pequenos, arborícolas, sem dimorfismo sexual e de hábitos noturnos. Sobre essa última característica, está relacionada com a evolução de seus olhos grandes, adaptados à ambientes com ausência parcial de luz.')
         .setImage('https://cdn.discordapp.com/attachments/774372894738219028/808954735423258634/unknown.png')
         .setFooter('NOTA: 5,17/10')
@@ -65,7 +65,49 @@ module.exports = {
         .setImage('https://cdn.discordapp.com/attachments/774372894738219028/808953646041399306/unknown.png')
         .setFooter('NOTA: 3,5/10')
 
-        var embeds = [embed1, embed2, embed3, embed4, embed5, embed6, embed7, embed8, embed9 ]
+        let embed10 = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Chimpanzé')
+        .setDescription('Como o homem, o chimpanzé consegue reconhecer a própria imagem no espelho, (capacidade que poucos animais apresentam). Também são capazes de aprender certos tipos de linguagens, como a dos sinais. Devido à destruição do seu habitat e à caça ilegal de chimpanzés pelo mercado de carne e de animais, pensa-se que restam apenas 150.000 chimpanzés nos bosques e florestas da África Central e Ocidental. Estima-se que no início do século XX havia aproximadamente dois milhões.')
+        .setImage('https://cdn.discordapp.com/attachments/809179193748226139/809183274357358592/unknown.png')
+        .setFooter('NOTA: 7,89/10')
+
+        let embed11 = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Uacari-Preto')
+        .setDescription('O Uacari-Preto é uma espécie nativa do Brasil que vive em grupos grandes, de até 100 indivíduos, em florestas tropicais. Ele está em perigo por causa da limitação do habitat, caça, e em alguns lugares está ameaçado pela presença de missões, grupos indígenas, garimpeiros e rodovias.')
+        .setImage('https://cdn.discordapp.com/attachments/809179193748226139/809183664990715904/unknown.png')
+        .setFooter('NOTA: 5/10')
+
+        let embed12 = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Uacari-Branco')
+        .setDescription('Locomove-se no alto dos galhos através de diferentes tipos de movimentos: utilizando-se dos quatro membros sem posição horizontal, saltado de galhos altos para galhos mais baixos e, verticalmente, escalando os troncos. Se alimenta de frutos, insetos, sementes, néctar e brotos de plantas. Uma característica importante é que eles adoram pular de galho em galho como os outros macacos.')
+        .setImage('https://cdn.discordapp.com/attachments/809179193748226139/809184443012349952/unknown.png')
+        .setFooter('NOTA: 3,12/10')
+
+        let embed13 = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Babuíno-Sagrado')
+        .setDescription('O babuino-sagrado possui um sistema social incomum de quatro níveis, chamado de sociedade de níveis multiplos. A minoria das interações sociais acontecem no interior de pequenos grupos, contendo um macho e até 10 fêmeas, chamados de haréns, que os macho lideram e protegem. Um harém frequentimente inclue um jovem macho "seguidor" que pode ser aparentado com o líder.')
+        .setImage('https://cdn.discordapp.com/attachments/809179193748226139/809185621368504320/unknown.png')
+        .setFooter('NOTA: 9/10')
+
+        let embed14 = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Callicebus')
+        .setDescription('Possuem costeletas, laterais do pescoço, garganta, face interior dos membros e partes inferiores do corpo uniformemente avermelhadas, contrastando com a coloração marrom-amarelado aguti do dorso, partes externas do tronco, base da cauda e coroa. Fronte em forma de coroa com coloração marrom-avermelhado aguti, bordeada por vibrissas superciliares pretas. Também são conhecidos pelos nomes de guigó, sauá, titi ou zogue-zogue')
+        .setImage('https://cdn.discordapp.com/attachments/809179193748226139/809184630242410556/unknown.png')
+        .setFooter('NOTA: 6,6/10')
+
+        let embed15 = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Cuxiú')
+        .setDescription('Estes animais têm hábitos diurnos e dieta frugíva, com duas espécie de origem amazônicas. Possuem pelagem espessa, principalmente na cauda não preênsil e na barba, com topete formado por dois tufos de pêlos. Costumam viver em bandos de até 30 indivíduos, consituindo um grupo ameaçado de extinção.')
+        .setImage('https://cdn.discordapp.com/attachments/809179193748226139/809185076671807488/unknown.png')
+        .setFooter('NOTA: 7/10')
+        
+        var embeds = [embed1, embed2, embed3, embed4, embed5, embed6, embed7, embed8, embed9, embed10, embed11, embed12, embed13, embed14, embed15 ]
         const random = embeds[Math.floor(Math.random() * embeds.length)];
 
         message.channel.send(random);
